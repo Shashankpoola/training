8a57414a1bfa32c05839b808c0c69ffeb0cf2735
MU473UU <MU473UU@IN3482453W1.ey.net>
2023-09-05 15:58:08 +0530

    mvn-demo

---
+++ mvn-demoo/src/main/java/BalanceException.java
@@ -0,0 +1,13 @@
+
+public class BalanceException extends Exception {
+
+	public BalanceException() {
+
+	}
+
+	public BalanceException(String message) {
+
+		super(message);
+	}
+
+}
---
+++ mvn-demoo/src/main/java/com/ey/Maths.java
@@ -0,0 +1,14 @@
+package com.ey;
+
+public class Maths {
+
+	int sum(int a, int b) {
+		return a + b;
+
+	}
+
+	int div(int a, int b) {
+		return a / b;
+	}
+
+}
